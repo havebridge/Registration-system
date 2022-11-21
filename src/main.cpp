d5792253955cb206ac68f971b5d1d9413b4531bc
havebridge <meyl0r@yandex.com>
2022-11-21 17:35:48 +0300

    window

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,72 @@
 
 #include "object.h"
 
+#if RELEASE
 
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
+#include <Windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR szCmdLing, int nCmdShow)
+{
+	MSG msg;
+	HWND hwnd;
+	WNDCLASSEX wc{ (sizeof WNDCLASSEX) };
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = reinterpret_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+	wc.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = [](HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)->LRESULT
+	{
+		switch (uMsg)
+		{
+			case WM_DESTROY:
+			{
+				PostQuitMessage(EXIT_SUCCESS);
+			}
+			return 0;
+		}
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	};
+	wc.lpszClassName = L"Window";
+	wc.lpszMenuName = nullptr;
+	wc.style = CS_VREDRAW | CS_HREDRAW;
+
+	if (!RegisterClassEx(&wc))
+	{
+		return EXIT_FAILURE;
+	}
+
+	if (hwnd = CreateWindow(wc.lpszClassName, L"Hasbridge", WS_OVERLAPPEDWINDOW, 100, 100, 600, 600, nullptr, nullptr, wc.hInstance, nullptr); hwnd == INVALID_HANDLE_VALUE)
+	{
+		return EXIT_FAILURE;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return static_cast<int>(msg.wParam);
+}
+
+
+#endif
+
+
+#if DEBUG
 int main(int argc, char** argv)
 {
-	const int tableSize = 20;
+	const int tableSize = 2;
 	HashTable::Hashtable<std::string, std::string, tableSize> Hashtable;
 	std::vector<HashTable::User<std::string, std::string>> Users;
 
@@ -21,7 +83,7 @@ int main(int argc, char** argv)
 
 	std::cout << Hashtable;
 
-	std::cout << Test::GetTest<std::string, std::string, tableSize>(Hashtable);
+	//std::cout << Test::GetTest<std::string, std::string, tableSize>(Hashtable);
 
 	/*ObjectModel::Object Test("test");
 
@@ -47,3 +109,4 @@ int main(int argc, char** argv)
 	ObjectModel::Object object = ObjectModel::Object::unpack(objectFromFile, it);*/
 	return 0;
 }
+#endif
