32a2ab66c0de9cb6747c5ff77efc1de53d481772
havebridge <91760176+havebridge@users.noreply.github.com>
2023-01-23 03:18:12 +0300

    Update main.h

--- src/user/main.h
+++ src/user/main.h
@@ -4,6 +4,8 @@ name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
 processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 
+#include "../window/win.h"
+
 #ifndef UNICODE
 #define UNICODE
 #endif 
